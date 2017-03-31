6dbd73a893384f1ee510ba73468d7b3118ec4140
Alex Hicks <aldahick@iupui.edu>
2017-03-31 12:04:41 -0400

    Fix cache-busting JS

--- inc/footer.php
+++ inc/footer.php
@@ -3,12 +3,16 @@
         <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-material-design/0.5.10/js/material.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-material-design/0.5.10/js/ripples.min.js"></script>
-        <?php if (isset($extra_js)): ?>
+        <?php
+        $version = get_current_commit_hash();
+        $cache = "?cacheVersion=$version";
+        if (isset($extra_js)):
+        ?>
             <?php foreach ($extra_js as $js_url): ?>
                 <script src="<?=$js_url . $cache?>"></script>
             <?php endforeach; ?>
         <?php endif; ?>
-        <script src="js/global.js<?$cache?>"></script>
+        <script src="js/global.js<?=$cache?>"></script>
         <?php
         // $pageName is defined in /inc/header.php
         $script_name = "js/" . strtolower($original_page_name) . ".js";

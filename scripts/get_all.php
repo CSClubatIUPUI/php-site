e2ead5ca15a15e6a02b2af99899c330e5120d9a0
Alex Hicks <alex@alexhicks.net>
2018-08-31 14:58:33 -0400

    Set up initial CircleCI configuration

---
+++ scripts/get_all.php
@@ -0,0 +1,22 @@
+<?php
+// outputs a newline-separated list of PHP files
+$php_dirs = [
+    ".",
+    "./inc",
+    "./post/get",
+    "./post/update"
+];
+$php_files = [];
+foreach ($php_dirs as $php_dir) {
+    $php_files = array_merge(
+        $php_files,
+        array_map(
+            function($filename) { global $php_dir; return $php_dir . "/" . $filename; },
+            array_filter(scandir(__DIR__ . "/../" . $php_dir), function($filename) {
+                return strpos($filename, ".php") !== false;
+            })
+        )
+    );
+}
+echo(implode("\n", $php_files) . "\n");
+?>
---
+++ scripts/lint.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+for filename in $(php -f ./scripts/get_all.php); do
+    php -l "$filename";
+done;
